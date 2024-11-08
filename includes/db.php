f5338ea79b44c320d66e53bc4b19f9ab874f5b13
Gabs051 <gabz15000@gmail.com>
2024-11-08 18:43:09 -0300

    feito registro e login

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,17 @@
-/*
-    Conexão com o banco de dados
-*/
+<?php 
+    /*
+        Conexão com o banco de dados
+    */
+
+    $DB_SERVER = "localhost";
+    $DB_USER = "root";
+    $DB_PASS = "";
+    $DB_NAME = "blog_db";
+
+    try {
+        $connect = new PDO("mysql:host=$DB_SERVER;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS);
+        $connect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e -> getMessage();
+    }
+?>
---
+++ views/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <h2>Cadastro de Usuário</h2>
+    <form action="../auth_process.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+
+        <label for="senha">Senha</label>
+        <input type="password" name="senha" id="senha" required minlength="12">
+
+        <button type="submit" name="register">Registrar</button>
+    </form>
+</body>
+</html>
